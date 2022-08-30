9a179a763711ba2588bddfe72f7e1f7f3f3794c0
Muhammad Farhan Ahmed <itsfarhan@outlook.com>
2022-08-30 07:44:54 -0400

    added a few folders
    More to come

---
+++ ch5/build/stereo/CMakeFiles/stereoVision.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stereoVision.dir/stereoVision.cpp.o"
+  "CMakeFiles/stereoVision.dir/stereoVision.cpp.o.d"
+  "stereoVision"
+  "stereoVision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stereoVision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
