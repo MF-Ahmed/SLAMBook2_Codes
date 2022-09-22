5dd595fb597e8495a17b6d9b83bc6dad82065f7e
Muhammad Farhan Ahmed <itsfarhan@outlook.com>
2022-09-22 18:26:10 +0200

    Added chapter 11
    removed some errors

---
+++ ch11/build/CMakeFiles/feature_training.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/feature_training.dir/feature_training.cpp.o"
+  "CMakeFiles/feature_training.dir/feature_training.cpp.o.d"
+  "feature_training"
+  "feature_training.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/feature_training.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
